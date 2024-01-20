515eee73cd588140a620919837c2932601ad1563
tongueEye <109783402+tongueEye@users.noreply.github.com>
2024-01-21 01:53:41 +0900

    * UI 디자인 및 화면 흐름 구현* 1. UI 디자인 (스플래시 화면, 시작화면, 게이트화면) 2. 화면 흐름 구현  - 스플래시 화면 이후 시작화면 띄움  - 시작 화면에서 play 버튼 클릭 시 게이트 화면으로 이동  - 시작 화면에서 quit 버튼 클릭 시 다이얼로그 창 띄우고 '예' 클리시 앱 종료  - 다이얼로그 창 디자인은 추후에 변경 예정

--- app/src/main/java/com/example/customquizapp/MainActivity.kt
+++ app/src/main/java/com/example/customquizapp/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.customquizapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import com.example.customquizapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +13,33 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.playBtn.setOnClickListener {
+            val intent = Intent(this, GateActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.quitBtn.setOnClickListener {
+            showExitDialog()
+        }
+    }
+
+    private fun showExitDialog(){
+        // 다이얼로그 생성
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("종료 확인")
+            .setMessage("앱을 종료하시겠습니까?")
+            .setPositiveButton("예") { _, _ ->
+                // '예' 버튼 클릭 시 앱 종료
+                finish()
+                Toast.makeText(this, "앱이 종료됩니다.", Toast.LENGTH_SHORT).show()
+            }
+            .setNegativeButton("아니오") { dialog, _ ->
+                // '아니오' 버튼 클릭 시 다이얼로그 닫기
+                dialog.dismiss()
+            }
+
+        // 다이얼로그 표시
+        builder.create().show()
     }
 }
