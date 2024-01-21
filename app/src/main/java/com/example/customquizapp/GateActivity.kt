836da56998383afb33ac431130d6d29df6793c39
tongueEye <109783402+tongueEye@users.noreply.github.com>
2024-01-22 01:45:05 +0900

    * 다이얼로그 창 커스텀 및 기능 구현* 1. 다이얼로그 창 커스텀  - 앱 종료 다이얼로그 창  - 문제집 추가 다이얼로그 창 2. UI 디자인  - 다이얼로그 창 디자인 3. 기능 구현  - 게이트 화면에 추가 버튼 클릭 시 문제집 추가 다이얼로그 창 뜨도록 구현 3. 애니메이션 파일 추가 (추후 필요시 사용예정)

--- app/src/main/java/com/example/customquizapp/GateActivity.kt
+++ app/src/main/java/com/example/customquizapp/GateActivity.kt
@@ -1,8 +1,14 @@
 package com.example.customquizapp
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.example.customquizapp.databinding.ActivityGateBinding
+import com.example.customquizapp.databinding.DialogCreateFolderBinding
 
 class GateActivity: AppCompatActivity() {
     private lateinit var binding: ActivityGateBinding
@@ -10,6 +16,38 @@ class GateActivity: AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityGateBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.addBtn.setOnClickListener {
+            showAddDialog()
+        }
+    }
+
+    private fun showAddDialog(){
+        //다이얼로그 뷰바인딩 초기화
+        val dialogBinding = DialogCreateFolderBinding.inflate(LayoutInflater.from(this))
+        //AlertDialog.Builder를 사용하여 다이얼로그 생성
+        val dialogBuilder = AlertDialog.Builder(this)
+        //AlertDialog 생성
+        val alertDialog = dialogBuilder.create()
+
+        //다이얼로그의 레이아웃 설정
+        alertDialog.setView(dialogBinding.root)
+
+        //다이얼로그의 배경을 투명색으로 설정
+        alertDialog.window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+        // "취소" 버튼 클릭 이벤트 설정
+        dialogBinding.cancelBtn.setOnClickListener {
+            alertDialog.dismiss() // 다이얼로그를 닫음
+        }
+
+        // "저장" 버튼 클릭 이벤트 설정
+        dialogBinding.saveBtn.setOnClickListener {
+            alertDialog.dismiss() // 다이얼로그 닫음
+            Toast.makeText(this, "새 문제집이 추가되었습니다.", Toast.LENGTH_LONG).show()
+        }
+        // 다이얼로그 표시
+        alertDialog.show()
 
     }
 }
--- app/src/main/java/com/example/customquizapp/MainActivity.kt
+++ app/src/main/java/com/example/customquizapp/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.customquizapp
 
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import com.example.customquizapp.databinding.ActivityMainBinding
+import com.example.customquizapp.databinding.DialogConfirmBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -17,6 +23,7 @@ class MainActivity : AppCompatActivity() {
         binding.playBtn.setOnClickListener {
             val intent = Intent(this, GateActivity::class.java)
             startActivity(intent)
+
         }
 
         binding.quitBtn.setOnClickListener {
@@ -25,21 +32,33 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun showExitDialog(){
-        // 다이얼로그 생성
-        val builder = AlertDialog.Builder(this)
-        builder.setTitle("종료 확인")
-            .setMessage("앱을 종료하시겠습니까?")
-            .setPositiveButton("예") { _, _ ->
-                // '예' 버튼 클릭 시 앱 종료
-                finish()
-                Toast.makeText(this, "앱이 종료됩니다.", Toast.LENGTH_SHORT).show()
-            }
-            .setNegativeButton("아니오") { dialog, _ ->
-                // '아니오' 버튼 클릭 시 다이얼로그 닫기
-                dialog.dismiss()
-            }
+        // 다이얼로그 뷰바인딩 초기화
+        val dialogBinding = DialogConfirmBinding.inflate(LayoutInflater.from(this))
+        // AlertDialog.Builder를 사용하여 다이얼로그 생성
+        val dialogBuilder = AlertDialog.Builder(this)
+        // AlertDialog 생성
+        val alertDialog = dialogBuilder.create()
 
+        // 다이얼로그의 레이아웃 설정
+        alertDialog.setView(dialogBinding.root)
+
+        // 다이얼로그 배경을 투명색으로 설정
+        alertDialog.window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+        // 다이얼로그 내용 설정
+        dialogBinding.confirmTextView.text = "종료하시겠습니까?"
+
+        // "취소" 버튼 클릭 이벤트 설정
+        dialogBinding.noButton.setOnClickListener {
+            alertDialog.dismiss() // 다이얼로그를 닫음
+        }
+
+        // "확인" 버튼 클릭 이벤트 설정
+        dialogBinding.yesButton.setOnClickListener {
+            alertDialog.dismiss() // 다이얼로그 닫음
+            finish() // 액티비티 종료
+        }
         // 다이얼로그 표시
-        builder.create().show()
+        alertDialog.show()
     }
 }
