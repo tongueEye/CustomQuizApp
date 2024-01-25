232c7f4a5862cadf0850a91f85a227641f93842e
tongueEye <109783402+tongueEye@users.noreply.github.com>
2024-01-26 03:13:24 +0900

    * ROOM DB 연동 및 Create, Read 기능 구현 1. UI 디자인 및 제작  - gate 화면에 리사이클러뷰 추가  - 리사이클러뷰 아이템 디자인 2. ROOM DB 연동  - folder table (id, name) 디자인  - CRUD 기능 위한 코드 작성 3. CREATE, READ 기능 구현  - 폴더 생성 창에서 데이터를 입력하면 해당 데이터가 ROOM DB(folder table)에 저장  - ROOM DB에 저장된 데이터를 읽어 gate 화면에 있는 리사이클러뷰에 뿌려줌

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    kotlin("kapt")
 }
 
 android {
@@ -47,4 +48,16 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    //room
+    val room_version = "2.6.1"
+
+    implementation("androidx.room:room-runtime:$room_version")
+    annotationProcessor("androidx.room:room-compiler:$room_version")
+    kapt("androidx.room:room-compiler:$room_version")
+    // optional - Kotlin Extensions and Coroutines support for Room
+    implementation("androidx.room:room-ktx:$room_version")
+
+    //Meterial
+    implementation("com.google.android.material:material:1.11.0")
 }
--- app/src/main/java/com/example/customquizapp/GateActivity.kt
+++ app/src/main/java/com/example/customquizapp/GateActivity.kt
@@ -1,17 +1,35 @@
 package com.example.customquizapp
 
+import android.app.Activity
+import android.content.Intent
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.room.Room
 import com.example.customquizapp.databinding.ActivityGateBinding
 import com.example.customquizapp.databinding.DialogCreateFolderBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.util.ServiceLoader
+
 
 class GateActivity: AppCompatActivity() {
     private lateinit var binding: ActivityGateBinding
+    private lateinit var folderAdapter: FolderAdapter
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityGateBinding.inflate(layoutInflater)
@@ -20,9 +38,32 @@ class GateActivity: AppCompatActivity() {
         binding.addBtn.setOnClickListener {
             showAddDialog()
         }
+
+        //RecyclerView 설정
+        binding.folderListRV.layoutManager = LinearLayoutManager(this)
+
+        //folderAdapter 초기화
+        folderAdapter = FolderAdapter()
+
+        //Adapter 적용
+        binding.folderListRV.adapter = folderAdapter
+
+        //폴더 목록 조회
+        loadFolderList()
     }
 
-    private fun showAddDialog(){
+    //폴더 목록 조회
+    private fun loadFolderList(){
+        val folderList: List<Folder> = AppDatabase.getDatabase(applicationContext)?.folderDao()?.getAllFolder()
+            ?: emptyList()
+
+        if (folderList.isNotEmpty()) {
+            // 데이터 적용
+            folderAdapter.setFolderList(folderList)
+        }
+    }
+
+    private fun showAddDialog() {
         //다이얼로그 뷰바인딩 초기화
         val dialogBinding = DialogCreateFolderBinding.inflate(LayoutInflater.from(this))
         //AlertDialog.Builder를 사용하여 다이얼로그 생성
@@ -43,11 +84,26 @@ class GateActivity: AppCompatActivity() {
 
         // "저장" 버튼 클릭 이벤트 설정
         dialogBinding.saveBtn.setOnClickListener {
-            alertDialog.dismiss() // 다이얼로그 닫음
-            Toast.makeText(this, "새 문제집이 추가되었습니다.", Toast.LENGTH_LONG).show()
+
+            val folderName = dialogBinding.folderNameEditText.text.toString()
+            if(folderName.isNotEmpty()){
+                alertDialog.dismiss() // 다이얼로그 닫음
+                Toast.makeText(this, "새 문제집이 추가되었습니다.", Toast.LENGTH_LONG).show()
+
+                // 폴더 생성
+                insertFolder(folderName)
+                loadFolderList()
+
+            } else{
+                Toast.makeText(this, "문제집 이름을 입력하세요!", Toast.LENGTH_LONG).show()
+            }
         }
         // 다이얼로그 표시
         alertDialog.show()
+    }
 
+    private fun insertFolder(name: String){
+        val folder = Folder(null, name)
+        AppDatabase.getDatabase(applicationContext)?.folderDao()?.insertFolder(folder)
     }
 }
